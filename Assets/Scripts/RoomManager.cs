44e9e8ee1229e9d90210ff637729f017f8e96cd5
gokmentas <56593104+gokmentas@users.noreply.github.com>
2023-10-09 11:05:28 +0300

    connected to the server

---
+++ Assets/Scripts/RoomManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Connecting...");
+
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+
+        Debug.Log("Connected to Server");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+
+        PhotonNetwork.JoinOrCreateRoom("test", null, null);
+
+        Debug.Log("We're connected and in a room now");
+    }
+
+} // class
